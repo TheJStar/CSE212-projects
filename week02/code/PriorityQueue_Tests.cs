6b160eaa2b50a5da075657a67c78e7bc5dae51b8
TheeJStar <kingoflightj@gmail.com>
2025-03-19 22:28:01 -0700

    added an other test method to ensure cde works

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -7,9 +7,9 @@ public class PriorityQueueTests
 {
     [TestMethod]
     // Scenario: the highest value person is returned
-    // Expected Result: 
+    // Expected Result: Tim
     // Defect(s) Found: 
-    public void TestPriorityQueue_1()
+    public void TestPriorityQueue_GetHighest()
     {
         var priorityQueue = new PriorityQueue();
         priorityQueue.Enqueue("Bob", 2);
@@ -22,9 +22,9 @@ public class PriorityQueueTests
 
     [TestMethod]
     // Scenario: if there are 2 (or more) highest values the first one in queue is returned
-    // Expected Result: 
-    // Defect(s) Found: 
-    public void TestPriorityQueue_2()
+    // Expected Result: Tim
+    // Defect(s) Found: was not getting to the last member of the queue, would Dequeue the last highest member
+    public void TestPriorityQueue_MoreThanOneHighest()
     {
         var priorityQueue = new PriorityQueue();
         priorityQueue.Enqueue("Bob", 2);
@@ -34,6 +34,36 @@ public class PriorityQueueTests
 
         var prioritizedValue = priorityQueue.Dequeue();
         Assert.AreEqual("Tim", prioritizedValue);
+    }
+
+    [TestMethod]
+    // Scenario: if there is nothing in the queue thwrow an Exception
+    // Expected Result: "The queue is empty."
+    // Defect(s) Found: 
+    public void TestPriorityQueue_Empty()
+    {
+        var priorityQueue = new PriorityQueue();
+        
+        try
+        {
+            priorityQueue.Dequeue();
+            Assert.Fail("Exception should have been thrown.");
+        }
+        catch (InvalidOperationException e)
+        {
+            Assert.AreEqual("The queue is empty.", e.Message);
+        }
+        catch (AssertFailedException)
+        {
+            throw;
+        }
+        catch (Exception e)
+        {
+            Assert.Fail(
+                 string.Format("Unexpected exception of type {0} caught: {1}",
+                                e.GetType(), e.Message)
+            );
+        }
     }
 
     // Add more test cases as needed below.
