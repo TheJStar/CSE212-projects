11320cb01a3ea172bd333833e30ae262aceb56b8
TheeJStar <kingoflightj@gmail.com>
2025-03-28 22:29:36 -0700

    fixed the test program

--- week04/code/LinkedList_Tests.cs
+++ week04/code/LinkedList_Tests.cs
@@ -148,10 +148,8 @@ public class RemoveTests
         Assert.AreEqual("<LinkedList>{5, 4, 35, 2, 2}", ll.ToString());
         ll.Remove(2);
         Assert.AreEqual("<LinkedList>{5, 4, 35, 2}", ll.ToString());
-        // ll.Remove(2);
-        // Assert.AreEqual("<LinkedList>{5, 4, 35}", ll.ToString());
-        ll.Remove(5);
-        Assert.AreEqual("<LinkedList>{4, 35, 2}", ll.ToString());
+        ll.Remove(2);
+        Assert.AreEqual("<LinkedList>{5, 4, 35}", ll.ToString());
     }
 }
 
